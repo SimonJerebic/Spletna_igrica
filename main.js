12decd95f73b9e43bccd1cccd7196a18dbcc83fc
SimonJerebic <simonerik.jerebic@gmail.com>
2025-06-14 00:58:33 +0200

    main.js

--- main.js
+++ main.js
@@ -1,75 +1,87 @@
+// Uvoz modulov za prednalaganje, meni in igralno sceno
 import Preload from "./preload.js";
 import MenuScene from "./menuscene.js";
 import GameScene from "./gamescene.js";
 
-const preload = new Preload();
-const menuscene = new MenuScene();
-const gamescene = new GameScene();
+// Ustvarjanje instanc posameznih scen
+const preload = new Preload(); // Instanca za prednalaganje virov
+const menuscene = new MenuScene(); // Instanca za meni sceno
+const gamescene = new GameScene(); // Instanca za igralno sceno
 
-gsap.defaults({ overwrite: "auto" });
-gsap.config({ nullTargetWarn: false });
-gsap.registerPlugin(CustomEase);
+// Nastavitve za GSAP animacijsko knjižnico
+gsap.defaults({ overwrite: "auto" }); // Samodejno prepisovanje obstoječih animacij
+gsap.config({ nullTargetWarn: false }); // Onemogoči opozorila za neveljavne cilje
+gsap.registerPlugin(CustomEase); // Registracija vtičnika za prilagojene animacijske krivulje
 
+// Konfiguracija Phaser igre
 const config = {
-    type: Phaser.AUTO,
-    mode: Phaser.Scale.RESIZE,
-    pixelArt: true,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    type: Phaser.AUTO, // Samodejno izbere WebGL ali Canvas renderer
+    mode: Phaser.Scale.RESIZE, // Omogoča prilagajanje velikosti platna
+    pixelArt: true, // Omogoči pixel art način za ostrejše slike
+    width: window.innerWidth, // Širina platna enaka širini okna
+    height: window.innerHeight, // Višina platna enaka višini okna
     physics: {
-        default: 'arcade',
+        default: 'arcade', // Uporaba arkadnega fizikalnega sistema
         arcade: {
-            gravity: { y: 50, x: 0 },
-            debug: false
+            gravity: { y: 50, x: 0 }, // Nastavitev gravitacije (samo po y-osi)
+            debug: false // Onemogoči prikaz fizikalnih podatkov za odpravljanje napak
         }
     },
-    parent: 'interactive',
-    scenes: [preload, menuscene, gamescene],
+    parent: 'interactive', // ID elementa, kjer bo igra prikazana
+    scenes: [preload, menuscene, gamescene], // Seznam scen, ki jih igra uporablja
     scale: {
-        mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: window.innerWidth,
-        height: window.innerHeight
+        mode: Phaser.Scale.RESIZE, // Prilagajanje velikosti glede na okno
+        autoCenter: Phaser.Scale.CENTER_BOTH, // Samodejno centriranje platna
+        width: window.innerWidth, // Ponovna nastavitev širine
+        height: window.innerHeight // Ponovna nastavitev višine
     },
-    backgroundColor: '#00000000' // Explicitly transparent
+    backgroundColor: '#00000000' // Prozorna barva ozadja
 };
 
+// Inicializacija Phaser igre z zgornjo konfiguracijo
 const game = new Phaser.Game(config);
 
-game.scene.add('preload', preload);
-game.scene.add('menuscene', menuscene);
-game.scene.add('gamescene', gamescene);
-game.scene.start('preload');
+// Dodajanje scen v igro
+game.scene.add('preload', preload); // Dodajanje prednalagalne scene
+game.scene.add('menuscene', menuscene); // Dodajanje meni scene
+game.scene.add('gamescene', gamescene); // Dodajanje igralne scene
+game.scene.start('preload'); // Začetek igre s prednalagalno sceno
 
+// Funkcija za zaznavanje mobilnih naprav
 function detectMobile() {
     var isMobile = false;
+    // Preverjanje user agent niza za zaznavanje mobilnih naprav
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
         || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x800|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
-    return isMobile;
+    return isMobile; // Vrne true, če je naprava mobilna, sicer false
 }
 
+// Funkcija za prilagajanje velikosti igre ob spremembi okna
 function resizeGame() {
-    const w = window.innerWidth; // Use full window width
-    const h = window.innerHeight;
+    const w = window.innerWidth; // Celotna širina okna
+    const h = window.innerHeight; // Celotna višina okna
 
-    game.scale.resize(w, h);
-    game.canvas.style.width = w + 'px';
-    game.canvas.style.height = h + 'px';
+    game.scale.resize(w, h); // Prilagodi velikost platna
+    game.canvas.style.width = w + 'px'; // Nastavi širino platna v CSS
+    game.canvas.style.height = h + 'px'; // Nastavi višino platna v CSS
 
+    // Prilagodi kamero za vse aktivne scene
     game.scene.scenes.forEach(scene => {
         if (scene.scene.isActive()) {
-            scene.cameras.main.setViewport(0, 0, w, h);
-            scene.cameras.main.centerOn(w / 2, h / 2);
+            scene.cameras.main.setViewport(0, 0, w, h); // Nastavi vidno polje kamere
+            scene.cameras.main.centerOn(w / 2, h / 2); // Centrira kamero
         }
     });
 
-    const isMobile = detectMobile();
+    const isMobile = detectMobile(); // Preveri, ali je naprava mobilna
     if (isMobile) {
-        document.body.classList.add("isMobile");
+        document.body.classList.add("isMobile"); // Doda razred za mobilne naprave
     } else {
-        document.body.classList.add("isDesktop");
+        document.body.classList.add("isDesktop"); // Doda razred za namizne naprave
     }
 }
 
+// Dodajanje poslušalca za dogodek spremembe velikosti okna
 window.addEventListener("resize", resizeGame);
-resizeGame(); // Trigger on load
+// Klic funkcije za prilagajanje ob nalaganju strani
+resizeGame();
